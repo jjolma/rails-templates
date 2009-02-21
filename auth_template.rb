ee956b50abd49101efbdaf0be3158e25d0b2b2a2
Jeff Jolma <jeff@animoto.com>
2009-02-21 16:35:40 -0500

    add sign up and login links to welcome page.  Nothing smart

--- auth_template.rb
+++ auth_template.rb
@@ -8,3 +8,15 @@ route "map.root :controller => 'welcome'"
 git :rm => "public/index.html"
 
 git :add => '.', :commit => "-m 'add welcome controller'"
+
+file "app/views/welcome/index.html.erb", <<-END
+  <p>
+    <%= link_to 'sign up', signup_path -%>
+  </p>
+  <p> 
+    <%= link_to 'log in', login_path -%>
+  </p>
+END
+git :add => '.', :commit => "-m 'link to sign in, sign up from welcome index'"
+
+
